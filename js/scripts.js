646e88fa6b026aa999ddf7d042903cba3c19f365
Kayl Eubanks <kayleubanks@gmail.com>
2018-03-23 13:10:08 -0700

    Fix conversion from array to string

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
+// holds user name
 var name = "";
-  // holds user name
-var suggestString = "";
   // holds string of letters used to determine answer via counter
-var experience = "";
+var suggestString = "";
   // holds no and little experience value
-var altExperience = "";
+var experience = "";
   // holds large experience value, use to eliminate letters from answer string
+var altExperience = "";
+  // define stringHandler as an array
+  var stringHandler = [];
 
 $(document).ready(function() {
   // Name and Experience logic
@@ -32,20 +34,19 @@ $(document).ready(function() {
   // Specific Experience logic
   $("#formExperience").submit(function(event) {
     if (experience === 'littleExp') {
-      // define suggestString as an index
-      suggestString = [];
-      // pull each value for checked boxes and compile into index called "temporary"
+      // pull each value for checked boxes and compile into array called "temporary"
       $(":checkbox:checked").each(function(temporary) {
-        // change value of suggestString to a string consisting of all values from index "temoporary"
-        suggestString[temporary] = $(this).val();
-        debugger
-        var stringHandler = "";
-        stringHandler = String.valueof(suggestString);
+        // change value of suggestString to a string consisting of all values from array "temoporary"
+        stringHandler[temporary] = $(this).val();
+        // converts stringHandler array to a string and assigns value to suggestString
+        suggestString = stringHandler.toString();
+        // // will replace every comma with nothing to create full string without commas
+        // suggestString = suggestString.replace(/,/g, '');
       });
     } else {
-      altExperience = [];
       $(":checkbox:checked").each(function(temporary) {
-        altExperience[temporary] = $(this).val();
+        stringHandler[temporary] = $(this).val();
+        altExperience = stringHandler.toString();
       });
     }
     console.log(suggestString);
