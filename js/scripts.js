812fb702861eab299024b30f8bf8f3e7117cdd64
Kayl Eubanks <kayleubanks@gmail.com>
2018-03-23 10:56:07 -0700

    change question to radio buttons on index page, add submit functions to scripts

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#formName").submit(fucntion(event) {
+    event.preventDefault();
+  });
+  $("#formExperience").submit(fucntion(event) {
+    event.preventDefault();
+  });
+  $("#formFocus").submit(fucntion(event) {
+    event.preventDefault();
+  });
+  $("#formWork").submit(fucntion(event) {
+    event.preventDefault();
+  });
+  $("#formFormat").submit(fucntion(event) {
+    event.preventDefault();
+  });
+});
